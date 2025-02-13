ff753123f40dc379bfc58b499d623dfee9238101
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-13 15:35:12 +0100

    restructure code and move more outside of try except block

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Standard library
+import glob
 import os
 import subprocess
 import sys
@@ -20,17 +21,20 @@ except ImportError as ie:
     left.write(f"could not import public_repository : {ie}")
 #
 right.header("Use a private repository")
+token = os.environ.get("token")
+if token:
+    st.toast(f"Token was found: {len(token)}")
+result = subprocess.Popen(
+    [(f'{sys.executable}'
+        " -m pip install "
+        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')],
+        shell=True)
+# wait for subprocess to install package before running your actual code below
+time.sleep(30)
+st.write(glob.glob("*"))
+time.sleep(30)
+st.write(glob.glob("*"))
 try:
-    token = os.environ.get("token")
-    if token:
-        st.toast(f"Token was found: {len(token)}")
-    result = subprocess.Popen(
-        [(f'{sys.executable}'
-          " -m pip install "
-          f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')],
-         shell=True)
-    # wait for subprocess to install package before running your actual code below
-    time.sleep(90)
     from private_repository import code as private_code
     private_code.make_dataframe(right)
     private_code.make_graph(right)
