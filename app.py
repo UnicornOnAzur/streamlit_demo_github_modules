0b9b27dcc9b2ac44eca84298c502338180158d04
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 09:53:29 +0100

    Show parent directory

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Standard library
-import glob
 import os
 import subprocess
 import sys
@@ -25,11 +24,19 @@ token = os.environ.get("token")
 if token:
     st.toast(f"Token was found: {len(token)}")
 right.write(os.listdir())
+# get current directory
+path = os.getcwd()
+right.write(f"Current Directory: {path}")
+ 
+# parent directory
+parent = os.path.dirname(path)
+right.write(f"Parent directory {parent}")
 result = subprocess.Popen(
     [(f'{sys.executable}'
-        " -m pip install --user "
-        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')],
-     shell=True)
+        " -m pip install "
+        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
+     ],
+    shell=True)
 # wait for subprocess to install package before running your actual code below
 time.sleep(30)
 right.write(os.listdir())
