0bb21a45777ebc6ae9423f081a30aaac4e8f04d6
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 11:19:27 +0100

    Test changing target dir

--- app.py
+++ app.py
@@ -23,16 +23,13 @@ except ImportError as ie:
 # RIGHT SIDE with the private repository
 right.header("Use a private repository")
 token = os.environ.get("token")  # get the token from the environment
-search_path = os.path.join(*[".."]*3, "**", "*")
-paths = glob.iglob(search_path, recursive=True)
-for path in paths:
-    right.write(path)
-# result = subprocess.Popen(
-#     [(f'{sys.executable}'
-#         " -m pip install "
-#         f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
-#      ],
-#     shell=True)
+result = subprocess.Popen(
+    [(f'{sys.executable}'
+        " -m pip install -- target"
+      f'{sys.executable}'  
+        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
+     ],
+    shell=True)
 # # wait for subprocess to install package before running your actual code below
 # time.sleep(30)
 # right.write(os.listdir())
