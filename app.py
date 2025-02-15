80b2c9c2d75dec14e96c59b1e9bec235e2bb933b
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 23:07:12 +0100

    Add one line to pyproject

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ token = os.environ.get("token")  # get the token from the environment
 parent_directory: str = os.path.dirname(os.getcwd())  # <TODO>
 with open(f"{parent_directory}/pyproject.toml", "w") as file:
     file.write("[project]")
+    file.write('name = "closed_repository"')
 result = subprocess.Popen(
     [(f'{sys.executable}'
         f" -m pip install -- target {parent_directory} "
