470399017aafd7b05de49211b4617d5433031787
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 22:33:50 +0100

    revert to changing target

--- app.py
+++ app.py
@@ -15,33 +15,28 @@ left, right = st.columns(2)  # Split the main page in two columns
 left.header("Use a public repository")
 try:
     from public_repository import code as public_code
-    public_code.make_dataframe(left)
-    public_code.make_graph(left)
+    public_code.create_empty_dataframe(left)
+    public_code.display_treemap(left)
 except ImportError as ie:
     left.write(f"could not import public_repository : {ie}")
 
 # RIGHT SIDE with the private repository
 right.header("Use a private repository")
 token = os.environ.get("token")  # get the token from the environment\
-right.write(sys.executable)
-right.write(os.getcwd())
-for p in glob.iglob(os.path.join(*[".."]*3, "**", "*"), recursive=True):
-    right.write(p)
-
-# result = subprocess.Popen(
-#     [(f'{sys.executable}'
-#         f" -m pip install -- target {os.path.dirname(os.getcwd())} "
-#         f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
-#      ],
-#     shell=True)
-# # # wait for subprocess to install package before running your actual code below
-# # time.sleep(30)
-# # right.write(os.listdir())
-# # time.sleep(30)
-# # right.write(os.listdir())
+result = subprocess.Popen(
+    [(f'{sys.executable}'
+        f" -m pip install -- target {os.path.dirname(os.getcwd())} "
+        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
+     ],
+    shell=True)
+# # wait for subprocess to install package before running your actual code below
+# time.sleep(30)
+# right.write(os.listdir())
+# time.sleep(30)
+# right.write(os.listdir())
 try:
     from private_repository import code as private_code
-    private_code.make_dataframe(right)
-    private_code.make_graph(right)
+    private_code.create_empty_dataframe(right)
+    private_code.display_treemap(right)
 except ImportError as ie:
     right.write(f"could not import private_repositoy : {ie}")
