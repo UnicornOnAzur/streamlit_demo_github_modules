db62f4553f940055f56f6f3e69f52faa7d5aa586
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 10:23:42 +0100

    Show parent directory

--- app.py
+++ app.py
@@ -22,30 +22,22 @@ except ImportError as ie:
 
 # RIGHT SIDE with the private repository
 right.header("Use a private repository")
-token = os.environ.get("token")
-if token:
-    st.toast(f"Token was found: {len(token)}")
-right.write(os.listdir())
-# get current directory
-path = os.getcwd()
-right.write(f"Current Directory: {path}")
+token = os.environ.get("token")  # get the token from the environment
 search_path = os.path.join(*[".."]*3, "**", "*.zip")
-paths = glob.glob(search_path, recursive=True)
-# parent directory
-parent = os.path.dirname(path)
-right.write(f"Parent directory {parent}")
-right.write(paths)
-result = subprocess.Popen(
-    [(f'{sys.executable}'
-        " -m pip install "
-        f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
-     ],
-    shell=True)
-# wait for subprocess to install package before running your actual code below
-time.sleep(30)
-right.write(os.listdir())
-time.sleep(30)
-right.write(os.listdir())
+paths = glob.iglob(search_path, recursive=True)
+for path in paths:
+    right.write(path)
+# result = subprocess.Popen(
+#     [(f'{sys.executable}'
+#         " -m pip install "
+#         f'git+https://{token}@github.com/UnicornOnAzur/closed_repository.git')
+#      ],
+#     shell=True)
+# # wait for subprocess to install package before running your actual code below
+# time.sleep(30)
+# right.write(os.listdir())
+# time.sleep(30)
+# right.write(os.listdir())
 try:
     from private_repository import code as private_code
     private_code.make_dataframe(right)
