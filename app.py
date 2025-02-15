9409df3ab734170bce52b87ef2062e18965d1f10
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-15 10:05:43 +0100

    Show recursive search

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Standard library
+import glob
 import os
 import subprocess
 import sys
@@ -6,11 +7,11 @@ import time
 # Third party
 import streamlit as st
 
+st.set_page_config(layout="wide")  # <>
+st.title("Demonstration of accessing Github repository")  # <>
+left, right = st.columns(2)  # Split the main page in two columns
 
-st.set_page_config(layout="wide")
-st.title("Demonstration of accessing Github repository")
-
-left, right = st.columns(2)
+# LEFT SIDE with the public repository
 left.header("Use a public repository")
 try:
     from public_repository import code as public_code
@@ -18,7 +19,8 @@ try:
     public_code.make_graph(left)
 except ImportError as ie:
     left.write(f"could not import public_repository : {ie}")
-#
+
+# RIGHT SIDE with the private repository
 right.header("Use a private repository")
 token = os.environ.get("token")
 if token:
@@ -27,10 +29,12 @@ right.write(os.listdir())
 # get current directory
 path = os.getcwd()
 right.write(f"Current Directory: {path}")
- 
+search_path = os.path.join(*[".."]*3, "**", "*.zip")
+paths = glob.glob(search_path, recursive=True)
 # parent directory
 parent = os.path.dirname(path)
 right.write(f"Parent directory {parent}")
+right.write(paths)
 result = subprocess.Popen(
     [(f'{sys.executable}'
         " -m pip install "
