b1ba1a54a4842f86e2261cef9e2607fd9389bcae
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-02-17 08:32:20 +0100

    gre

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ parent_directory: str = os.path.dirname(os.getcwd())  # <TODO>
 with open(f"{parent_directory}/pyproject.toml", "w") as file:
     file.write("[project]\n")
     file.write('name = "closed_repository"\n')
-    file.write('version = "0.0.1"\n')
+    file.write('version = "0.0.1a"\n')
 result = subprocess.Popen(
     [(f'{sys.executable}'
         f" -m pip install -- target {parent_directory} "
